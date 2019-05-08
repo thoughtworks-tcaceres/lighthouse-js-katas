ab16eed51c2c4a553bcfb58e931e07f5e1ff6f51
Tyler Caceres <tyler.caceres@gmail.com>
2019-05-08 19:36:18 +0000

    conditional-sum.js file added

---
+++ conditional-sum.js
@@ -0,0 +1,17 @@
+const conditionalSum = function(values, condition) {
+  // Your code here
+  var sum = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (condition === "even" && values[i] % 2 === 0) {
+      sum += values[i];
+    } else if (condition === "odd" && values[i] % 2 !== 0) {
+      sum += values[i];
+    }
+  }
+  return sum;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
